cef89c9ab5c240fb6be3d702ffda9f749e7ab6d0
AlexCraciun <56975249+alxcraciun@users.noreply.github.com>
2023-05-25 09:50:42 +0300

    LocalStorage + Date

---
+++ js/script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+    var timeNow = new Date();
+    var datetime = "Session Started at: "
+        + timeNow.getDate()
+        + "/" + (timeNow.getMonth() + 1) + "/"
+        + timeNow.getFullYear() + " @ "
+        + timeNow.getHours() + ":"
+        + timeNow.getMinutes() + ":"
+        + timeNow.getSeconds();
+    localStorage.setItem('last_session', timeNow);
+};
