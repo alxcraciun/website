412c739ea75cd18acc05d663050e5dc9a9cfbd9d
AlexCraciun <56975249+alxcraciun@users.noreply.github.com>
2023-05-25 10:50:14 +0300

    Form Submissions + addDeskSetup

---
+++ js/addDeskSetup.js
@@ -0,0 +1,24 @@
+function addDeskSetup() {
+  var newDeskSetupBox = `
+    <div class="desk-setup-box">
+      <img src="images/desk-setup.jpg" alt="">
+      <div class="desk-setup-layer">
+        <h4>Artist Name</h4>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, placeat.</p>
+        <a href="#"><i class="bx bx-link-external"></i></a>
+      </div>
+    </div>
+  `;
+
+  var btnAddDeskSetup = document.getElementById("btnAddDeskSetup");
+  btnAddDeskSetup.insertAdjacentHTML("beforebegin", newDeskSetupBox);
+}
+
+var btnAddDeskSetup = document.getElementById("btnAddDeskSetup");
+btnAddDeskSetup.addEventListener("click", addDeskSetup);
+
+
+
+
+
+
---
+++ js/formSubmission.js
@@ -0,0 +1,18 @@
+document.getElementById('newsletter-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nameInput = document.getElementById('name-input');
+    const emailInput = document.getElementById('email-input');
+
+    // check if I have at least one character before & after @ and after the dot
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(emailInput.value);
+
+    if (!isValidEmail) {
+        alert('Please enter a valid email address!');
+        return;
+    }
+
+    nameInput.value = '';
+    emailInput.value = '';
+});
